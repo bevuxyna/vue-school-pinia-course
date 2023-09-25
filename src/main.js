5d4cbff5242a0cd7b222a5a7724ca50d8176b640
vmelnikova <vmelnikova@phoenixit.ru>
2023-09-25 17:07:21 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,17 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { createPinia } from "pinia";
-
-// Icons and Styles
 import FontAwesomePlugin from "./plugins/FontAwesome";
 import "./assets/main.pcss";
-
-// App Wide Components
 import AppButton from "./components/AppButton.vue";
 import AppCountInput from "./components/AppCountInput.vue";
 import AppModalOverlay from "./components/AppModalOverlay.vue";
 
-// Init App
 createApp(App)
   .use(createPinia())
   .use(FontAwesomePlugin)
