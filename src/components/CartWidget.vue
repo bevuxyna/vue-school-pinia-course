c7d192634a14528e68172e64b03619334d830575
vmelnikova <vmelnikova@phoenixit.ru>
2023-09-25 18:09:30 +0300

    fix

--- src/components/CartWidget.vue
+++ src/components/CartWidget.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import {ref} from "vue";
 import CartItem from "./CartItem.vue";
 import {useCartStore} from "@/stores/CartStore";
 
@@ -10,45 +10,43 @@ const cartStore = useCartStore();
 <template>
   <div class="relative">
     <span class="cursor-pointer" @click="active = true">
-      <fa icon="shopping-cart" size="lg" class="text-gray-700" />
+      <fa icon="shopping-cart" size="lg" class="text-gray-700"/>
       <div class="cart-count absolute">{{ cartStore.count }}</div>
     </span>
     <AppModalOverlay :active="active" @close="active = false">
       <div v-if="!cartStore.isEmpty">
         <ul class="items-in-cart">
           <CartItem
-            :product="{ name: 'Dried Pineapple', price: 5 }"
-            :count="5"
-            @updateCount=""
-            @clear=""
-          />
-          <CartItem
-            :product="{ name: 'Pineapple Gum', price: 3 }"
-            :count="5"
-            @updateCount=""
-            @clear=""
+              v-for="(items, name) in cartStore.grouped"
+              :key="name"
+              :product="items[0]"
+              :count="cartStore.groupCount(name)"
+              @updateCount=""
+              @clear="cartStore.clearItem(name)"
           />
         </ul>
         <div class="flex justify-end text-2xl mb-5">
-          Total: <strong>$40</strong>
+          Total: <strong>${{ cartStore.total }}</strong>
         </div>
         <div class="flex justify-end">
-          <AppButton class="secondary mr-2">Clear Cart</AppButton>
+          <AppButton class="secondary mr-2" @click="cartStore.$reset()">Clear Cart</AppButton>
           <AppButton class="primary">Checkout</AppButton>
         </div>
       </div>
-       <div v-else><em>Cart is Empty</em></div>
+      <div v-else><em>Cart is Empty</em></div>
     </AppModalOverlay>
   </div>
 </template>
 <style lang="pcss" scoped>
-.items-in-cart{
+.items-in-cart {
   @apply mb-5;
 }
-.items-in-cart li{
+
+.items-in-cart li {
   @apply flex justify-between p-2;
 }
-.items-in-cart li:nth-of-type(even){
+
+.items-in-cart li:nth-of-type(even) {
   @apply bg-gray-300;
 }
 </style>
--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -1,17 +1,16 @@
 import { defineStore } from "pinia";
+import { groupBy } from "lodash";
 
 export const useCartStore = defineStore('CartStore', {
     state: () => ({
         items: []
     }),
     getters: {
-        count() {
-            return this.items.length;
-        },
-        isEmpty() {
-            return this.count === 0;
-        }
-
+        count: (state) => state.items.length,
+        isEmpty: (state) => state.count === 0,
+        grouped: (state) => groupBy(state.items, (item) => item.name),
+        groupCount: (state) => (name) => state.grouped[name].length,
+        total: (state) => state.items.reduce((previous, current) => previous + current.price, 0),
     },
     actions: {
         addItem(count, item){
@@ -19,7 +18,9 @@ export const useCartStore = defineStore('CartStore', {
             for (let i = 0; i < count; i++) {
                this.items.push({ ...item });
             }
-
+        },
+        clearItem(itemName) {
+            this.items = this.items.filter(item => item.name !== itemName);
         }
     }
 });
